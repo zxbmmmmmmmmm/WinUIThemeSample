fbe195b1e1544d88c5701f71b86ad2a9c46f43ce
zxbmm <zxbmm@DESKTOP-WIN11>
2023-06-23 01:31:27 +0800

    添加项目文件。

---
+++ WinUIThemeTest/Theme.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+using WinUIThemeTest;
+
+namespace WinUIThemeTest
+{
+    internal partial class Theme:ResourceDictionary
+    {
+        public Theme()
+        {
+            this.InitializeComponent();
+            if (Environment.OSVersion.Version.Build >= 22000)//判断系统是否为Win11
+            {
+                this.MergedDictionaries.Add(new XamlControlsResources { ControlsResourcesVersion = ControlsResourcesVersion.Version2 });//添加新版本控件样式
+            }
+            else
+            {
+                this.MergedDictionaries.Add(new XamlControlsResources { ControlsResourcesVersion = ControlsResourcesVersion.Version1 });//添加旧版本控件样式
+            }
+        }
+    }
+}
